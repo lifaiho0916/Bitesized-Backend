4b038eecf5003a98020038218b179357e0372307
PhoenixDev1223 <phoenixdev1223@gmail.com>
2022-10-13 13:47:41 +0800

    Update

--- client/src/constants/language.tsx
+++
@@ -1,5 +0,0 @@
-import { EN } from "./language/en";
-import { CH } from "./language/ch";
-export {
-    EN, CH
-};
--- client/src/pages/error/error404.tsx
+++
@@ -1,6 +0,0 @@
-const Error404 = () => {
-    window.location.replace('https://www.creatogether.app/error404');
-    return null;
-}
-
-export default Error404;
