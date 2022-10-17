447b3f4f1efb6877b1297e0ed5fd80d1d8cbfeec
PhoenixDev1223 <phoenixdev1223@gmail.com>
2022-10-17 22:48:36 +0800

    code clean

--- src/models/GeneralSetting.ts
+++
@@ -1,9 +0,0 @@
-// import mongoose, { Schema } from "mongoose";
-
-// const GeneralSetting = new mongoose.Schema({
-//   referralLinkDonuts: {
-//     type: Number
-//   }
-// });
-
-// export default mongoose.model("generalsettings", GeneralSetting);
