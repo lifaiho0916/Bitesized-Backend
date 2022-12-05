0c091d602efed9cc0a40983a5d884e7410ecd207
PhoenixDev1223 <phoenixdev1223@gmail.com>
2022-12-05 10:21:14 +0800

    Pop up warning- Remove card

--- src/controllers/paymentController.ts
+++ src/controllers/paymentController.ts
@@ -2,11 +2,21 @@ import mongoose from 'mongoose'
 import Stripe from "stripe"
 import User from '../models/User'
 import Payment from '../models/Payment'
+import Subscriber from '../models/Subscriber'
+import Subscription from '../models/Subscription'
+import Setting from '../models/Setting'
 
 const stripe = new Stripe(
     `${process.env.STRIPE_SECRET_KEY}`,
     { apiVersion: '2022-11-15', typescript: true }
 )
+
+const calcTime = () => {
+    var d = new Date()
+    var utc = d.getTime()
+    var nd = new Date(utc + (3600000 * 8))
+    return nd
+}
 
 export const getPayment = async (req: any, res: any) => {
     try {
@@ -58,9 +68,24 @@ export const deleteCard = async (req: any, res: any) => {
     try {
         const { userId } = req.body
         const payment: any = await Payment.findOne({ user: userId })
+        const currentTime = calcTime()
         if (payment) {
+            const setting: any = await Setting.findOne()
+            const currencyRate = JSON.parse(setting.currencyRate)
             await stripe.customers.del(payment.stripe.customerId)
             await Payment.findByIdAndDelete(payment._id)
+            const subscribers = await Subscriber.find({ user: userId, status: true })
+            subscribers.forEach(async (sub: any) => {
+                stripe.subscriptions.del(sub.subscriptionId)
+                const plan: any = await Subscription.findOne({ productId: sub.productId })
+                Subscriber.findByIdAndUpdate(sub._id, {
+                    benefits: plan.benefits,
+                    planName: plan.name,
+                    price: JSON.parse(plan.multiPrices)[`${sub.currency}`] * 1.034 + 0.3 * (sub.currency === 'usd' ? 1.0 : currencyRate[`${sub.currency}`]),
+                    status: false,
+                    cancelledAt: currentTime
+                }).exec()
+            })
         } else return res.status(200).json({ success: false })
 
         return res.status(200).json({ success: true })
